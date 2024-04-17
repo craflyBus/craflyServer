db0534ef445e75bd80a4876b3b1c94abe933bfb3
dogbokchif <cksgur0612@dgsw.hs.kr>
2024-04-17 13:47:32 +0900

    spring init

---
+++ src/main/java/com/crafly/craflyserver1/CraflyServer1Application.java
@@ -0,0 +1,13 @@
+package com.crafly.craflyserver1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CraflyServer1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CraflyServer1Application.class, args);
+    }
+
+}

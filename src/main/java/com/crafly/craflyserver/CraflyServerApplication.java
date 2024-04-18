e9a9311b1c72dfa17493e36e4015a62815590886
dogbokchif <cksgur0612@dgsw.hs.kr>
2024-04-18 09:00:25 +0900

    properties 연결

--- src/main/java/com/crafly/craflyserver/CraflyServerApplication.java
+++ src/main/java/com/crafly/craflyserver/CraflyServerApplication.java
@@ -1,7 +1,9 @@
 package com.crafly.craflyserver;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 public class CraflyServerApplication {

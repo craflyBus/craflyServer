5723839308024f9545f0b0def7da4e6dfa49640e
dogbokchif <cksgur0612@dgsw.hs.kr>
2024-04-23 16:02:36 +0900

    webAdapter 어노테이션 오류 해결

--- src/main/kotlin/com/crafly/craflyserver/util/annotation/WebAdapter.kt
+++ src/main/kotlin/com/crafly/craflyserver/util/annotation/WebAdapter.kt
@@ -2,14 +2,13 @@ package com.crafly.craflyserver.util.annotation
 
 import org.springframework.core.annotation.AliasFor
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.ResponseBody
 import org.springframework.web.bind.annotation.RestController
 
 @Target(AnnotationTarget.CLASS)
 @Retention(AnnotationRetention.RUNTIME)
 @MustBeDocumented
 @RestController
-@ResponseBody
+@RequestMapping
 annotation class WebAdapter(
     @get:AliasFor(annotation = RequestMapping::class, attribute = "path")
     val path: String = ""
